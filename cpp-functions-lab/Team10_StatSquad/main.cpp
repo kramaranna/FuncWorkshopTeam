f87426c01492bd6fec72fdd17cd8c6a8001e6d45
GKovtonyuk <galyna.kovtonyuk@gmail.com>
2025-10-27 23:24:01 +0200

    Файли проєктів

--- cpp-functions-lab/Team10_StatSquad/main.cpp
+++ cpp-functions-lab/Team10_StatSquad/main.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int findElement(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++)
-        if (arr[i] == key)
-            return i;
-    return -1;
-}
-
 int main() {
     int n, arr[100], key;
     cout << "Введіть кількість елементів: ";
     cin >> n;
+
     cout << "Введіть елементи: ";
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
     cout << "Введіть число для пошуку: ";
     cin >> key;
-    int index = findElement(arr, n, key);
+
+    int index = -1; 
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            index = i;
+            break; 
+        }
+    }
+
     if (index != -1)
         cout << "Елемент знайдено на позиції " << index + 1;
     else
         cout << "Елемент не знайдено.";
+
     return 0;
 }
