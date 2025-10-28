109621cf7189ad612d59d1497baf5e776e7274a7
kramaranna <kramaranna123@gmail.com>
2025-10-28 12:23:44 +0200

    Додано функцію

--- cpp-functions-lab/Team5_ArrayNinjas/main.cpp
+++ cpp-functions-lab/Team5_ArrayNinjas/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int arr[5];
-    cout << "Введіть 5 чисел: ";
-    for (int i = 0; i < 5; i++) cin >> arr[i];
+
+void findMinMax(int arr[], int size) {
     int minVal = arr[0], maxVal = arr[0];
-    for (int i = 1; i < 5; i++) {
+    for (int i = 1; i < size; i++) {
         if (arr[i] < minVal) minVal = arr[i];
         if (arr[i] > maxVal) maxVal = arr[i];
     }
     cout << "Мінімум: " << minVal << ", Максимум: " << maxVal;
+}
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE];
+    cout << "Введіть " << SIZE << " чисел: ";
+    for (int i = 0; i < SIZE; i++) cin >> arr[i];
+
+    findMinMax(arr, SIZE);
+
+    return 0;
 }
